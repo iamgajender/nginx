a0ae45b201f6d8d5a5544bcae2bed8a6251be457
gajender <iamgajender0707@gmail.com>
2026-06-14 06:46:38 +0000

    adding the changes in the app files

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ def api_users():
     return {
         "users": [
             {"id": 1, "name": "John"},
-            {"id": 2, "name": "David"}
+            {"id": 2, "name": "David"},
+            {"id": 3, "name": "Sanju"}
         ]
     }
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000)
